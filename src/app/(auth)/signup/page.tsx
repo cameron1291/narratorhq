4fe7faa23f8fc8f28cb15f3db7461f37a8cef45a
Cameron <cameron@Camerons-MacBook-Pro.local>
2026-05-20 15:37:34 +1000

    Force dynamic rendering on signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react'
 import SignupForm from './signup-form'
 
+export const dynamic = 'force-dynamic'
+
 export default function SignupPage() {
   return (
     <Suspense>
